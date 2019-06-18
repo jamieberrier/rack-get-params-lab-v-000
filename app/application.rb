b1126265f4ed8ba51f166fdebe4352a9ea92798c
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 19:46:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,8 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
-
+      @@cart.each do |item|
+        resp.write "#{item}\n"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)

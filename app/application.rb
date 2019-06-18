8db88ead64a1b604f29ed4b463a957ea43892efe
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 19:52:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -41,7 +41,7 @@ class Application
 
   def handle_add(add_item)
     if @@items.include?(add_item)
-      
+      @@cart << add_item
     else
       return "#{add_item} is already in the cart"
     end

dc4ca4076e6c74f2c97b0cbd00e38ee07f155dc8
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 19:52:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
-
+      add_item = req.params["item"]
     else
       resp.write "Path Not Found"
     end
